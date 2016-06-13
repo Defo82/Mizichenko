4ca4690f74f4042e66a77967181b68035a6c33e8
Defo82 <de.fo@mail.ru>
2016-06-13 23:43:44 +0300

    Update Triangle.java

--- lesson3/triangle/Triangle.java
+++ lesson3/triangle/Triangle.java
@@ -1,5 +1,4 @@
-package lesson3.triangle;
-public class Triangle {
+package lesson3.triangle;public class Triangle {
     public Point a, b,c;
 
     public Triangle(Point a, Point b, Point c) {
@@ -7,26 +6,25 @@ public class Triangle {
         this.b = b;
         this.c = c;
     }
-       public boolean isExists () {
+    public boolean isExists () {
       /*
       Возможны два случая когда нельзя построить треугольник:
       1. Все точки лежат на одной прямой.
       2. Координаты двух точек совпадают.
       */
 
-        boolean result = true;
 
-        //Уравнение прямой для трех точек
-        result = ((this.c.x - this.a.x) / (this.b.x - this.a.x) == (this.c.y - this.a.y) / (this.b.y - this.a.y))?false:true;
-
-        //Проверяем координаты
-        result = (this.a.x == this.b.x && this.a.y == this.b.y)?false:true;
-        result = (this.a.x == this.c.x && this.a.y == this.c.y)?false:true;
-        result = (this.c.x == this.b.x && this.c.y == this.b.y)?false:true;
-
-        return result;
-
+        if (
+            //Уравнение прямой для трех точек
+            (this.c.x - this.a.x) / (this.b.x - this.a.x) == (this.c.y - this.a.y) / (this.b.y - this.a.y) ||
+            //Проверяем координаты
+            (this.a.x == this.b.x && this.a.y == this.b.y) ||
+            (this.a.x == this.c.x && this.a.y == this.c.y) ||
+            (this.c.x == this.b.x && this.c.y == this.b.y)
+                )return false;
+        else return true;
     }
+
 
     public double area() {
         //calculate the triangle area
