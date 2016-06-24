43827e783363052ba5e77c311cae13d3199e2a65
defo82 <de.fo@mail.ru>
2016-06-24 11:00:20 +0300

    save

---
+++ lesson5/duplicate/Main.java
@@ -0,0 +1,36 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        RD rd = new RD();
+
+        String[] arr = {"слово0",
+                        "слово1",
+                        "слово2",
+                        "слово3",
+                        "слово1",
+                        "слово2",
+                        "слово4",
+                        "слово5",
+                        "слово6"};
+
+        for (String a : arr) {
+            System.out.println(a+"\t");
+        }
+
+
+        rd.removeDuplicate(arr);
+
+
+        System.out.println("\n"+"");
+        for (String a : arr) {
+            System.out.println(a+"\t");
+        }
+
+
+
+
+
+
+    }
+}
---
+++ lesson5/duplicate/RD.java
@@ -0,0 +1,24 @@
+
+public class RD {
+
+    public void removeDuplicate(String[] values) {
+
+        /*
+        метод должен найти дубликат
+        и сдвинуть все последующие элементы влево затерев тем самым его,
+        последний элемент занулить
+        */
+
+        for (int i = 0; i < values.length-1; i++){
+            for (int j = i+1; j < values.length; j++){
+                if (values[i].equals(values[j])){
+                    for (int k = j; k < values.length - 1; k++){
+                        values[k] = values[k+1];
+                    }
+                    values[values.length - 1] = "0";// немогу занулить NullPointerException
+                }
+            }
+
+        }
+    }
+}
