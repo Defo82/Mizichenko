c09f83a059a58203298920079ee37b7873002357
defo82 <de.fo@mail.ru>
2016-06-25 07:09:08 +0300

    all gonna be allrihgt

---
+++ test/Main.java
@@ -0,0 +1,14 @@
+package test;
+public class Main {
+    public static void main(String[] args) {
+
+        String origin = "Qui bene distinguit, bene docet";
+        String sub =             "distinguit";
+
+        SubWord one = new SubWord();
+
+
+        System.out.println(one.contains(origin, sub));
+
+    }
+}
