d39a75d83cacc2e22edd451a2e42069b8a89b4cb
defo82 <de.fo@mail.ru>
2016-07-11 01:40:49 +0300

    ok

--- lesson7/src/main/java/ru/amizichenko/tracker/start/StartUITest.java
+++ lesson7/src/main/java/ru/amizichenko/tracker/start/StartUITest.java
@@ -5,9 +5,9 @@ public class StartUITest {
 	public static void main(String[] args) {
 
 		Input input = new StubInput(new String[] {	"1","1","Bart","a son",
-			"1","Lisa","a daughter",
-			"1","Homer","loving beer",
-			"4","6"});
+													"1","Lisa","a daughter",
+													"1","Homer","loving beer",
+													"4","6"});
 		new StartUI(input).init();
 
 
