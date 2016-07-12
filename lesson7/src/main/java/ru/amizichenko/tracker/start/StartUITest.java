80b589119ccd4b93ddc454f98ff538db958b31e5
Defo <de.fo@mail.ru>
2016-07-12 04:52:56 +0300

    Update StartUITest.java

--- lesson7/src/main/java/ru/amizichenko/tracker/start/StartUITest.java
+++ lesson7/src/main/java/ru/amizichenko/tracker/start/StartUITest.java
@@ -4,7 +4,10 @@ public class StartUITest {
 
 	public static void main(String[] args) {
 
-		Input input = new StubInput(new String[] {"1","1","a","b","4","6"});
+		Input input = new StubInput(new String[] {  "1","1","Bart","a son",
+ 								"1","Lisa","a daughter",
+								"1","Homer","loving beer",
+								"4","6"});
 		new StartUI(input).init();
 
 
