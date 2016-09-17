36666b473d54ca95c90eb9d93ab07dca3627a47d
defo82 <de.fo@mail.ru>
2016-09-17 19:01:18 +0300

    prime numbers

---
+++ part5/lesson11/src/main/java/ru/amizichenko/tracker/services/IteratorPrimeNumbers.java
@@ -0,0 +1,57 @@
+package ru.amizichenko.tracker.services;
+
+import java.util.Iterator;
+
+/**
+ * Created by Defo on 14.09.2016.
+ */
+public class IteratorPrimeNumbers implements Iterator {
+
+    private final int[] values;
+    private int index = 0;
+
+    public IteratorPrimeNumbers(int[] values) {
+        this.values = values;
+    }
+
+
+    public boolean hasNext() {
+        boolean check = false;
+        for (int i = this.index; i < this.values.length; i++) {
+            if(isPrime(values[i])) {
+                check = true;
+                break;
+            } else check = false;
+        }
+        return check;
+    }
+
+    public Object next() {
+        int primeNumber = 0;
+        for (int i = this.index; i < this.values.length; i++) {
+            if (isPrime(values[i])) {
+                primeNumber = values[i];
+                this.index = i+1;
+                break;
+            }
+        }
+
+        return primeNumber;
+    }
+
+    public void remove() {
+    }
+
+    private boolean isPrime(int n){
+        boolean prime = true;
+        if( n == 1 ) prime = false; // 1 - не простое число
+
+        // перебираем возможные делители от 2 до sqrt(n)
+        for(int i=2; i*i<=n; i++){
+            // если разделилось нацело, то составное
+            if(n % i ==0) prime = false;
+        }
+        // если нет нетривиальных делителей, то простое
+        return prime;
+    }
+}
--- part5/lesson11/src/test/java/ru/amizichenko/tracker/services/IteratorPrimeNumbersTest.java
+++ part5/lesson11/src/test/java/ru/amizichenko/tracker/services/IteratorPrimeNumbersTest.java
@@ -6,28 +6,30 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 /**
- * Created by Defo on 11.09.2016.
+ * Created by Defo on 17.09.2016.
  */
-public class IteratorEvenNumbersTest {
+public class IteratorPrimeNumbersTest {
 
     @Test
-    public void whenGetNextCallShouldReturnEvenNumberAndPointerForward() {
-        IteratorEvenNumbers it = new IteratorEvenNumbers(new int[] {1,1,1,2,1,1,4});
+    public void whenGetNextCallShouldReturnPrimeNumberAndPointerForward() {
+        IteratorPrimeNumbers it = new IteratorPrimeNumbers(new int[] {1,1,1,2,1,1,3});
         it.next();
         int resoult = (Integer) it.next();
 
-        assertThat(resoult, is(4));
+        assertThat(resoult, is(3));
     }
 
     @Test
     public void whenCheckNextPositionShouldReturnContentValue() {
-        IteratorEvenNumbers it = new IteratorEvenNumbers(new int[] {1,1,1,2,1,1,4});
+        IteratorPrimeNumbers it = new IteratorPrimeNumbers(new int[] {1,1,1,2,1,1,3});
         it.next();
         it.next();
+
         boolean resoult = it.hasNext();
 
         assertThat(resoult, is(false));
     }
+
 
 
 }
