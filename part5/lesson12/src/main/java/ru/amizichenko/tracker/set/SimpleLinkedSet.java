450d56699ffeb1bfe465f1754e587b371283c3e5
defo82 <de.fo@mail.ru>
2017-03-20 13:03:25 +0300

    Signed-off-by: defo82 <de.fo@mail.ru>

--- part5/lesson12/src/main/java/ru/amizichenko/tracker/set/SimpleLinkedSet.java
+++ part5/lesson12/src/main/java/ru/amizichenko/tracker/set/SimpleLinkedSet.java
@@ -9,36 +9,81 @@ public class SimpleLinkedSet<E> {
     private Wrap<E> last;
     private int size;
 
-    public void add() {
-
+    /**
+     * Добавить новый объект в список уникальных элементов
+     * @param e новый элемент
+     */
+    public void add(E e) {
+        if (!checkDuplicate(e)) {
+            if (this.first == null) {
+                this.first = this.last = new Wrap<>(null, e, null );
+            } else {
+                Wrap<E> shift = this.last;
+                this.last = new Wrap<>(shift, e, null);
+                shift.next = this.last;
+            }
+        }
     }
 
-    private boolean checkDuplicate() {
-        return false;
+    /**
+     * Проверить объект на уникальность
+     * @param e новый объект
+     * @return true если найдено совпадение
+     */
+    private boolean checkDuplicate(E e) {
+        boolean check = false;
+        Iterator iterator = iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().equals(e)) check = true;
+        }
+        return check;
     }
 
+    /**
+     * Возвращает объект итератора
+     * @return
+     */
     public Iterator<E> iterator() {
         return new Iterator<E>();
     }
 
 
-
+    /**
+     * Обертка для элемента
+     * Так же содержит ссылки на предыдущий и следующий объекты
+     * @param <E>
+     */
         private class Wrap<E> {
             Wrap<E> pre;
-            Wrap<E> current;
+            E current;
             Wrap<E> next;
+
+            Wrap(Wrap<E> pre, E current, Wrap<E> next){
+                this.pre = pre;
+                this.current = current;
+                this.next = next;
+
+            }
         }
 
+    /**
+     * Итератор
+     * @param <E>
+     */
         private class Iterator<E> implements java.util.Iterator<E> {
+            private Wrap<E> lastElement = (Wrap<E>) first;
+            private int iterIndex = 0;
 
             @Override
             public boolean hasNext() {
-                return false;
+                return iterIndex < size;
             }
 
             @Override
             public E next() {
-                return null;
+                Wrap<E> element = lastElement;
+                lastElement = element.next;
+                return (E) element;
             }
         }
 }
--- part5/lesson12/src/test/java/ru/amizichenko/tracker/set/SimpleSetTest.java
+++ part5/lesson12/src/test/java/ru/amizichenko/tracker/set/SimpleSetTest.java
@@ -1,12 +1,9 @@
 package ru.amizichenko.tracker.set;
 
-import org.junit.Assert;
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
-import ru.amizichenko.tracker.lists.SimpleArrayList;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
 
 /**
  * Created by Defo on 03.03.2017.
@@ -14,8 +11,7 @@ import static org.junit.Assert.*;
 public class SimpleSetTest {
 
     @Test
-
-    public void add() throws Exception {
+    public void WhenScrollThrowSetEachElementIsUnique() throws Exception {
         SimpleSet<Integer> simpleSet = new SimpleSet<>(10);
         simpleSet.add(1);
         simpleSet.add(1);
@@ -28,7 +24,7 @@ public class SimpleSetTest {
         simpleSet.next();                   //2
         int check = simpleSet.next();       //3
 
-        assertThat(check, is(3));
+        MatcherAssert.assertThat(check, is(3));
         }
 }
 
